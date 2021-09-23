da2f1c6465d1ed8b55e049a18a452ce26bebef6b
arthur.escriou <arthur.escriou@gmail.com>
2021-09-23 21:22:04 +0200

    save code

--- src/handler.js
+++ src/handler.js
@@ -1,7 +1,7 @@
 'use strict'
 const AWS = require('aws-sdk')
 const S3 = new AWS.S3()
-const Bucket = ''
+const Bucket = 'svb-41-dev'
 
 const putObject = ({ path, content }) =>
   S3.putObject({ Bucket, Key: path, Body: content })
@@ -9,12 +9,16 @@ const putObject = ({ path, content }) =>
 const compile = (code) => code
 
 module.exports.compile = async (event) => {
-  const { code, user } = JSON.parse(event.body)
+  const { code, user, name } = JSON.parse(event.body)
+
+  const compiled = compile(code)
+  await new Promise.all([
+    putObject({ path: `${user}/${name}-compiled`, content: compiled }),
+    putObject({ path: `${user}/${name}`, content: code }),
+  ])
+
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      code,
-      user,
-    }),
+    body: JSON.stringify(compiled),
   }
 }
