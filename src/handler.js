23a36bef8497a1397247432b080b9f56282ef898
arthur.escriou <arthur.escriou@gmail.com>
2021-09-23 21:11:27 +0200

    add s3

--- src/handler.js
+++ src/handler.js
@@ -1,13 +1,20 @@
 'use strict'
+const AWS = require('aws-sdk')
+const S3 = new AWS.S3()
+const Bucket = ''
 
-module.exports.compile = async (event) => ({
-  statusCode: 200,
-  body: JSON.stringify(
-    {
-      message: 'Go Serverless v2.0! Your function executed successfully!',
-      input: event,
-    },
-    null,
-    2
-  ),
-})
+const putObject = ({ path, content }) =>
+  S3.putObject({ Bucket, Key: path, Body: content })
+
+const compile = (code) => code
+
+module.exports.compile = async (event) => {
+  const { code, user } = JSON.parse(event.body)
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      code,
+      user,
+    }),
+  }
+}
