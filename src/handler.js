c3d0b8f993bb87ef12227777a077fea09ae2c6a0
arthur.escriou <arthur.escriou@gmail.com>
2021-09-23 20:00:45 +0200

    :tada: initial commit

---
+++ src/handler.js
@@ -0,0 +1,13 @@
+'use strict'
+
+module.exports.compile = async (event) => ({
+  statusCode: 200,
+  body: JSON.stringify(
+    {
+      message: 'Go Serverless v2.0! Your function executed successfully!',
+      input: event,
+    },
+    null,
+    2
+  ),
+})
