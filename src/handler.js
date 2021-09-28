7ab6b61c3320ee5de1309ebfcf157209759346ec
arthur.escriou <arthur.escriou@gmail.com>
2021-09-28 14:07:45 +0200

    handle errors

--- src/handler.js
+++ src/handler.js
@@ -12,19 +12,29 @@ const putObject = ({ path, content }) =>
 
 module.exports.compile = async event => {
   const { code, uid, name } = JSON.parse(event.body)
-  const compiled = await compile(code, uid, name)
-  console.log(
-    await Promise.all([
-      putObject({ path: `${uid}/${name}-compiled`, content: compiled }),
-      putObject({ path: `${uid}/${name}`, content: code }),
-    ])
-  )
-  const body = JSON.stringify(compiled)
-  return {
-    statusCode: 200,
-    body,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-    },
+  try {
+    const compiled = await compile(code, uid, name)
+    console.log(
+      await Promise.all([
+        putObject({ path: `${uid}/${name}-compiled`, content: compiled }),
+        putObject({ path: `${uid}/${name}`, content: code }),
+      ])
+    )
+    const body = JSON.stringify(compiled)
+    return {
+      statusCode: 200,
+      body,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+    }
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: err.message,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+    }
   }
 }
