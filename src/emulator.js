ce68925e3331ff6768f6f1ddf499d758d7d8a1dc
arthur.escriou <arthur.escriou@gmail.com>
2022-02-21 15:31:24 +0100

    add log to emulator

--- src/emulator.js
+++ src/emulator.js
@@ -6,6 +6,12 @@ const emulator = () => {
     if (request.method !== 'POST') return { statusCode: 404 }
     const event = { body: request.body }
     const context = {} // awsRequestId: ''
+    try {
+      const data = JSON.parse(event.body)
+      console.log('compiling', data.uid, data.name)
+    } catch (e) {
+      /*balec*/
+    }
     return handler.compile(event, context)
   })
   console.log('Server started')
