3d1e43cbf175f40ed21e8293f1a7a4610df18b90
Guillaume Hivert <hivert.is.coming@gmail.com>
2022-03-14 18:11:58 +0100

    Upgrade svb-41/engine and core

--- test/assets/assault.ts
+++ test/assets/assault.ts
@@ -1,12 +1,11 @@
 import * as svb from '@svb-41/core'
-const { dist2 } = svb.helpers
 
 type Data = { initialDir?: number }
-type ControllerArgs = svb.controller.ControllerArgs<Data>
-const FIRE = svb.controller.Instruction.FIRE
+type Context = svb.Context<Data>
+const FIRE = svb.Instruction.FIRE
 
 export const initialData: Data = {}
-export default ({ stats, radar, memory, ship }: ControllerArgs) => {
+export default ({ stats, radar, memory, ship }: Context) => {
   if (!memory.initialDir) memory.initialDir = stats.position.direction
   if (stats.position.speed < 0.1) return ship.thrust()
 
@@ -14,24 +13,20 @@ export default ({ stats, radar, memory, ship }: ControllerArgs) => {
     const isSameTeam = res.team === stats.team
     if (!isSameTeam) return false
     const source = stats.position
-    const target = svb.helpers.nextPosition(200)(res.position)
-    const finalAngle = svb.helpers.angle({ source, target })
+    const target = svb.geometry.nextPosition(200)(res.position)
+    const finalAngle = svb.geometry.angle({ source, target })
     const direction = finalAngle - stats.position.direction
     return Math.abs(direction) < 0.1
   })
 
-  const closeEnemy = radar
-    .filter(res => res.team !== stats.team && !res.destroyed)
-    .map(res => ({ res, dist: dist2(res.position, stats.position) }))
-
-  if (closeEnemy.length > 0) {
-    const nearestEnemy = closeEnemy.reduce((a, v) => (a.dist > v.dist ? v : a))
+  const near = svb.radar.nearestEnemy(radar, stats.position)
+  if (near) {
     const source = stats.position
-    const target = nearestEnemy.res.position
-    const threshold = 4 / Math.sqrt(nearestEnemy.dist)
+    const target = near.enemy.position
+    const threshold = 4 / Math.sqrt(near.dist2)
     const speed = stats.weapons[0]?.bullet.position.speed
-    const delay = Math.sqrt(nearestEnemy.dist) / speed
-    const resAim = svb.helpers.aim({ ship, source, target, threshold, delay })
+    const delay = Math.sqrt(near.dist2) / speed
+    const resAim = svb.geometry.aim({ ship, source, target, threshold, delay })
     if (resAim.id === FIRE && ally) return ship.idle()
     return resAim
   }
--- test/assets/fail.ts
+++ test/assets/fail.ts
@@ -1,27 +1,25 @@
 import * as svb from '@svb-41/core'
 
 type Data = { initialDir: number }
-type ControllerArgs = svb.controller.ControllerArgs<Data>
+type Context = svb.Context<Data>
 
 export const initialData = { initialDir: 0 }
-export default (args: ControllerArgs) => {
+export default (args: Context) => {
   const { stats, memory, comm, radar, ship } = args
   if (!memory.initialDir) memory.initialDir = stats.position.direction
   if (stats.position.speed < 0.08) return ship.thrust()
   if (radar.length > 0) {
-    const enemies = radar
-      .filter(res => res.team !== stats.team && !res.destroyed)
-      .map(res => res.position)
+    const enemies = svb.radar.closeEnemies(radar, stats.position)
     if (enemies.length > 0) comm.sendMessage(enemies)
     const importantTarget = radar
       .filter(res => res.team !== stats.team && !res.destroyed)
-      .find(enemy => enemy.size === '16')
+      .find(enemy => enemy.size === 16)
     if (importantTarget) {
-      const targetDist = svb.helpers.dist2(
+      const targetDist = svb.geometry.dist2(
         stats.position,
         importantTarget!.position
       )
-      return svb.helpers.aim({
+      return svb.geometry.aim({
         ship,
         source: stats.position,
         target: importantTarget.position,
