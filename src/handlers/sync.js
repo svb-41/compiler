47441aac567179f552b2fc09d1eb0554ab7ac88e
Guillaume Hivert <hivert.is.coming@gmail.com>
2022-04-13 16:52:33 +0200

    Test lambda

--- src/handlers/sync.js
+++ src/handlers/sync.js
@@ -1,7 +1,11 @@
 const AWS = require('aws-sdk')
 
-const db = new AWS.DynamoDB()
+const db = new AWS.DynamoDB.DocumentClient()
 
 module.exports.sync = async (event, context) => {
-  return { statusCode: 200 }
+  const Key = { username: 'guillaume' }
+  const Item = { ...Key, coucou: 'cestmoua' }
+  await db.put({ TableName: 'preferences', Item }).promise()
+  const res = await db.get({ TableName: 'preferences', Key }).promise()
+  return { statusCode: 200, body: JSON.stringify(res.Item) }
 }
