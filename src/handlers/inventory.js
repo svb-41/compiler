5ab7ea02adead1c2dbb2ef011783128f65ae58f9
arthur.escriou <arthur.escriou@gmail.com>
2022-05-18 01:29:03 +0200

    :bug: inventory on error (or not created)

--- src/handlers/inventory.js
+++ src/handlers/inventory.js
@@ -1,11 +1,17 @@
 const AWS = require('../services/aws')
 const auth = require('../services/auth')
 
+const getUserInventory = async id => {
+  const res = await AWS.DynamoDB.inventory.get(id)
+  if (res === null) return { items: [], favoritesItems: [] }
+  return res
+}
+
 module.exports.inventory = async (event, context) => {
   try {
     const au = event.headers.Authorization ?? event.headers.authorization ?? ''
     const username = await auth.get(au.slice(7))
-    const data = await AWS.DynamoDB.inventory.get(username)
+    const data = await getUserInventory(username)
     const inventory = await Promise.all(data.items.map(AWS.DynamoDB.stuff.get))
     return { statusCode: 200, body: JSON.stringify({ ...data, inventory }) }
   } catch (error) {
